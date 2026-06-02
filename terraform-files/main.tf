9f3c0a222864d49587e0c990048b5eac0ad2cfd9
naveenleon <vikiwayne3437@gmail.com>
2026-06-02 18:37:27 +0530

    terra added

---
+++ terraform-files/main.tf
@@ -0,0 +1,45 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-south-1a", "ap-south-1b", ]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", ]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", ]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 21.0"
+
+  name               = "eks_cluster"
+  kubernetes_version = "1.33"
+
+  # Optional
+  endpoint_public_access = true
+
+  # Optional: Adds the current caller identity as an administrator via cluster access entry
+  enable_cluster_creator_admin_permissions = true
+
+  compute_config = {
+    enabled    = true
+    node_pools = ["general-purpose"]
+  }
+
+  vpc_id     = "module.vpc.vpc_id"
+  subnet_ids = ["module.vpc.private_subnets", "module.vpc.public_subnets"]
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
