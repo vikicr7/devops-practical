a4367bdd365e84f173277c5e833c148f7d220319
naveenleon <vikiwayne3437@gmail.com>
2026-06-02 18:03:17 +0530

    help

--- dockerfiles/app.py
+++ dockerfiles/app.py
@@ -7,7 +7,7 @@ class Handler(http.server.BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'text/plain; charset=utf-8')
             self.end_headers()
-            self.wfile.write(b"Hello from DevOps Assessment!")
+            self.wfile.write(b"Hello from DevOps Assessment")
         else:
             self.send_response(404)
             self.end_headers()
